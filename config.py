90a2ae525934d98dcc2bcd259e01368e003bc295
Cedric GESTES <ctaf42@gmail.com>
2011-01-05 01:05:45 +0100

    initial config (mostly from roger-config.py)

---
+++ config.py
@@ -0,0 +1,149 @@
+from libqtile.manager import Key, Click, Drag, Screen, Group
+from libqtile.command import lazy
+from libqtile import layout, bar, widget, hook
+
+mod = 'mod4'
+# The bindings below are for use with a Kinesis keyboard, and may not make
+# sense for standard keyboards.
+keys = [
+    # First, a set of bindings to control the layouts
+    Key(
+        [mod], "k",
+        lazy.layout.down()
+    ),
+    Key(
+        [mod], "j",
+        lazy.layout.up()
+    ),
+    Key(
+        [mod, "control"], "k",
+        lazy.layout.shuffle_down()
+    ),
+    Key(
+        [mod, "control"], "j",
+        lazy.layout.shuffle_up()
+    ),
+    Key(
+        [mod], "space",
+        lazy.layout.next()
+    ),
+    Key(
+        [mod, "shift"], "space",
+        lazy.layout.rotate()
+    ),
+    Key(
+        [mod, "shift"], "Return",
+        lazy.layout.toggle_split()
+    ),
+
+    Key([mod], "h",      lazy.to_screen(1)),
+    Key([mod], "l",      lazy.to_screen(0)),
+    # ~/bin/x starts a terminal program
+    Key([mod], "Tab",    lazy.nextlayout()),
+    Key([mod], "w",      lazy.window.kill()),
+    Key([mod], "F2",     lazy.spawn(
+        "dmenu_run -p run -nb '#202020' -nf '#ffffff' -fa 'Anonymous Pro-10'")),
+
+    Key(
+        [mod, "shift"], "k",
+        lazy.spawn("amixer -c 0 -q set Master 2dB+")
+    ),
+    Key(
+        [mod, "shift"], "j",
+        lazy.spawn("amixer -c 0 -q set Master 2dB-")
+    ),
+    Key(
+        [mod], "Left", lazy.group.prevgroup(),
+    ),
+    Key(
+        [mod], "Right", lazy.group.nextgroup(),
+    ),
+]
+
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+        start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+        start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+# Next, we specify group names, and use the group name list to generate an appropriate
+# set of bindings for group switching.
+groups = [
+    Group("1"),
+    Group("2"),
+]
+for i in groups:
+    keys.append(
+        Key(["mod4"], i.name, lazy.group[i.name].toscreen())
+    )
+    keys.append(
+        Key(["mod4", "shift"], i.name, lazy.window.togroup(i.name))
+    )
+
+
+# Two simple layout instances:
+layouts = [
+    layout.DecoratedStack(),
+    layout.Max(),
+    layout.Stack(stacks=2),
+    layout.Tile(),
+]
+
+
+screens = [
+    Screen(
+        top = bar.Bar(
+                    [
+                        widget.GroupBox(borderwidth=2,
+                            fontsize=14,
+                            padding=1, margin_x=1, margin_y=1),
+                        widget.Sep(),
+                        widget.WindowName(
+                            fontsize=14, margin_x=6),
+                        widget.Sep(),
+                        widget.CPUGraph(width=50, graph_color='0066FF',
+                                                  fill_color='001188'),
+                        widget.MemoryGraph(width=50, graph_color='22FF44',
+                                                     fill_color='11AA11'),
+                        widget.SwapGraph(width=50, graph_color='FF2020',
+                                                   fill_color='C01010'),
+                        widget.Sep(),
+                        widget.Systray(),
+                        widget.Sep(),
+                        widget.Clock('%H:%M %d/%m/%y',
+                            fontsize=18, padding=6),
+                    ],
+                    24
+                ),
+    ),
+]
+
+def main(qtile):
+    from dgroups import DGroups, Match
+
+    #'www':  {'init': True, 'persist': True, 'spawn': ''},
+    groups = {
+            'h4x':  {'init': True, 'persist': True, 'spawn': 'gnome-terminal'},
+            'design': {},
+            'emesene': {},
+            'gajim': {},
+           }
+    apps = [
+            {'match': Match(wm_class=['Gimp']),
+                'group': 'design', 'float': True},
+            {'match': Match(wm_class=['emesene']),
+                'group': 'emesene'},
+            {'match': Match(wm_class=['Gajim.py']),
+                'group': 'gajim'},
+            {'match': Match(wm_class=['Guake.py', 'MPlayer'],
+                wm_type=['dialog']), 'float': True},
+            {'match': Match(wm_class=['Wine']), 'float': True, 'group': 'wine'},
+           ]
+    dgroups = DGroups(qtile, groups, apps)
+
+@hook.subscribe.client_new
+def dialogs(window):
+    if window.window.get_wm_type() == 'dialog':
+        window.floating = True
---
+++ dgroups.py
@@ -0,0 +1,97 @@
+import libqtile.hook
+
+
+class Match(object):
+    ''' Match for dynamic groups
+        it can match by title, class or role '''
+    def __init__(self, title=[], wm_class=[], role=[], wm_type=[]):
+        self._rules = [('title', t) for t in title]
+        self._rules += [('wm_class', w) for w in wm_class]
+        self._rules += [('role', r) for r in  role]
+        self._rules += [('wm_type', r) for r in  wm_type]
+
+    def compare(self, client):
+        for _type, rule in self._rules:
+            match_func = getattr(rule, 'match', None) or\
+                         getattr(rule, 'count')
+
+            if _type == 'title':
+                value = client.name
+            elif _type == 'wm_class':
+                value = client.window.get_wm_class()
+                if value:
+                    value = value[1]
+            elif _type == 'wm_type':
+                value = client.window.get_wm_type()
+            else:
+                value = client.window.get_wm_window_role()
+
+            if value and match_func(value):
+                return True
+        return False
+
+
+class DGroups(object):
+    ''' Dynamic Groups '''
+    def __init__(self, qtile, groups, apps, config=None):
+        self.qtile = qtile
+
+        self.groups = groups
+        self.apps = apps
+
+        self.config = config
+
+        self._setup_hooks()
+        self._setup_groups()
+
+    def _setup_groups(self):
+        for name, tag in self.groups.iteritems():
+            if tag.get('init') == True:
+                self.qtile.addGroup(name)
+
+            spawn_cmd = tag.get('spawn')
+            if spawn_cmd:
+                self.qtile.cmd_spawn(spawn_cmd)
+
+    def _setup_hooks(self):
+        libqtile.hook.subscribe.client_new(self._add)
+        libqtile.hook.subscribe.client_killed(self._del)
+
+    def _add(self, client):
+        for app in self.apps:
+            # Matching Rules
+            if app['match'].compare(client):
+                if 'group' in app:
+                    group = app['group']
+                    self.qtile.addGroup(group)
+                    client.togroup(group)
+
+                if 'float' in app and app['float']:
+                    client.floating = True
+                return
+
+        # Unmatched
+        current_group = self.qtile.currentGroup.name
+        if current_group in self.groups and\
+                self.groups[current_group].get('exclusive'):
+
+            wm_class = client.window.get_wm_class()
+
+            if wm_class:
+                group_name = wm_class[1]
+            else:
+                group_name = client.name
+
+            self.qtile.addGroup(group_name)
+            client.togroup(group_name)
+
+    def _del(self, client):
+        group = client.group
+
+        # Delete group if empty and no persist
+        if not (group.name in self.groups and\
+           self.groups[group.name].get('persist')) and\
+                               len(group.windows) == 1:
+
+            client.group.remove(client)
+            self.qtile.delGroup(group.name)
